3d9769f7064e6c53b1f8852a9a635758b82f4589
Suraj Thakkar <suraj.gthakkar@gmail.com>
2025-03-09 13:29:07 +0530

    changed vite to let it run webp files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/',
   server: {
-    host: true, // Expose to all network interfaces
+    host: true,
   },
+  publicDir: 'public',
+  assetsInclude: ['**/*.webp'],
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 });
